e6ca811a0758b0c600f6f5e9255cd56d25dace19
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2021-12-23 18:00:16 +0100

    add sections 4 and 5

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// setInterval(dataSwitcher, 6000);
+
+// function dataSwitcher() {
+//     const grid1 = document.querySelector('#grid1');
+//     const grid2 = document.querySelector('#grid2');
+
+//     grid1.style
+// }
