0cd7778fbc1b0d0dad8b02f28ccfbcb024938e18
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2022-01-06 12:45:46 +0100

    add dark mode switcher

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+/* funzioni da implementare:
+  - funzionamento dark mode switcher
+    - cambio colore e icone switcher
+    - scroll bar in dark mode
+    - background bianchi in dark mode
+    - testi in dark purple in white
+    - background win millions
+    - background in earn section
+    - bacground e testi in used by
+  - tabella dinamica in earn section
+  - modal per setting
+  - modal per connect wallet
+  - cambio iniziali con selettore lingua
+*/
+
 // setInterval(dataSwitcher, 6000);
 
 // function dataSwitcher() {
